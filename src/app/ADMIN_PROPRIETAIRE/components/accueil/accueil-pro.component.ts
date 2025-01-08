791d47445628efec85d78900c586beb46f70c297
Mapathe Ndiaye <mapathendiaye542@gmail.com>
2025-01-08 18:10:39 +0000

    Ajout de la partie supprimer etablissement avec le popup Ajout des notifications suite à un bloquage avec la date et la fin du bloquage

--- src/app/ADMIN_PROPRIETAIRE/components/accueil/accueil-pro.component.ts
+++ src/app/ADMIN_PROPRIETAIRE/components/accueil/accueil-pro.component.ts
@@ -4,10 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { MatIcon } from '@angular/material/icon';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
 
-
-
 interface Client {
-  id:number;
+  id: number;
   nom: string;
   pays: string;
   statut: string;
@@ -25,7 +23,6 @@ export interface Comment {
   replies: Comment[];
 }
 
-
 @Component({
   selector: 'app-accueil-pro',
   standalone: true,
@@ -35,193 +32,202 @@ export interface Comment {
     MatIcon,
     CommonModule,
     MatTabGroup,
-    MatTab
+    MatTab,
   ],
   templateUrl: './accueil-pro.component.html',
-  styleUrl: './accueil-pro.component.css'
+  styleUrl: './accueil-pro.component.css',
 })
-
 export class AccueilProComponent implements OnInit, OnDestroy {
   ngOnInit(): void {}
 
   ngOnDestroy(): void {}
 
-
-    clients: Client[] = [
+  clients: Client[] = [
     {
       nom: 'Jean Dupont',
       statut: 'Hotellerrie',
       duree: '45 jours',
-      pays: "Gabon",
+      pays: 'Gabon',
       type_propriete: '',
       evaluation: 'Très bon',
-      id: 1
+      id: 1,
     },
     {
       nom: 'Marie Curie',
       statut: 'Tourisme',
       duree: '5 jours',
-      pays: "Gabon",
-      type_propriete: "",
+      pays: 'Gabon',
+      type_propriete: '',
       evaluation: 'Très bon',
-      id: 2
+      id: 2,
     },
     {
       nom: 'Pierre Martin',
       statut: 'Hotellerrie',
       duree: '5 jours',
-      pays: "Sénégal",
+      pays: 'Sénégal',
       type_propriete: '',
       evaluation: 'bon',
-      id: 3
+      id: 3,
     },
     {
       nom: 'Sophie Germain',
       statut: 'Hotellerrie',
       duree: '5 jours',
-      pays: "Bénin",
+      pays: 'Bénin',
       type_propriete: '',
       evaluation: 'Moins bon',
-      id: 4
+      id: 4,
     },
     {
       nom: 'Lucie Aubrac',
       statut: 'Tourisme',
       duree: '5 jours',
-      pays: "RDC",
+      pays: 'RDC',
       type_propriete: '',
       evaluation: 'Mauvais',
-      id: 5
+      id: 5,
     },
     {
       nom: 'Antoine Doinel',
       statut: '2024-11-02',
       duree: '5 jours',
-      pays: "",
+      pays: '',
       type_propriete: '',
       evaluation: '',
-      id: 6
+      id: 6,
     },
     {
       nom: 'Clara Zetkin',
       statut: '2024-11-11',
       duree: '3 jours',
-      pays: "",
+      pays: '',
       type_propriete: '',
       evaluation: '',
-      id: 7
+      id: 7,
     },
     {
       nom: 'Gabriel Garcia',
       statut: '2024-10-30',
       duree: '3 jours',
-      pays: "",
+      pays: '',
       type_propriete: '',
       evaluation: '',
-      id: 8
+      id: 8,
     },
     {
       nom: 'Nina Simone',
       statut: '2024-11-03',
       duree: '3 jours',
-      pays: "",
+      pays: '',
       type_propriete: '',
       evaluation: '',
-      id: 9
+      id: 9,
     },
     {
       nom: 'Albert Camus',
       statut: '2024-11-12',
       duree: '7 jours',
-      pays: "",
+      pays: '',
       type_propriete: '',
       evaluation: '',
-      id: 10
+      id: 10,
     },
-    ];
+  ];
 
-    images: string[] = [
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-      'assets/img/chambre.png',
-    ];
+  images: string[] = [
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+    'assets/img/chambre.png',
+  ];
 
-      comments: Comment[] = [
-        {
-          id: 1,
-          userId: 'Utilisateur1',
-          content: 'déception totale !',
-          createdDate: new Date('2024-11-20'),
-          rating: 1,
-          replies: []
-        },
-        {
-          id: 2,
-          userId: 'Utilisateur2',
-          content: 'Le service client est très réactif.',
-          createdDate: new Date('2024-11-21'),
-          rating: 4,
-          replies: []
-        },
-        {
-          id: 3,
-          userId: 'Utilisateur3',
-          content: 'Je recommande vivement ce site.',
-          createdDate: new Date('2024-11-22'),
-          rating: 5,
-          replies: []
-        },
-        {
-          id: 4,
-          userId: 'Utilisateur4',
-          content: 'Un peu déçu par le service client.',
-          createdDate: new Date('2024-11-23'),
-          rating: 3,
-          replies: []
-        },
-        {
-          id: 5,
-          userId: 'Utilisateur5',
-          content: 'très belles chambres, j\'adore!.',
-          createdDate: new Date('2024-11-24'),
-          rating: 4,
-          replies: []
-        }
-      ];
+  comments: Comment[] = [
+    {
+      id: 1,
+      userId: 'Utilisateur1',
+      content: 'déception totale !',
+      createdDate: new Date('2024-11-20'),
+      rating: 1,
+      replies: [],
+    },
+    {
+      id: 2,
+      userId: 'Utilisateur2',
+      content: 'Le service client est très réactif.',
+      createdDate: new Date('2024-11-21'),
+      rating: 4,
+      replies: [],
+    },
+    {
+      id: 3,
+      userId: 'Utilisateur3',
+      content: 'Je recommande vivement ce site.',
+      createdDate: new Date('2024-11-22'),
+      rating: 5,
+      replies: [],
+    },
+    {
+      id: 4,
+      userId: 'Utilisateur4',
+      content: 'Un peu déçu par le service client.',
+      createdDate: new Date('2024-11-23'),
+      rating: 3,
+      replies: [],
+    },
+    {
+      id: 5,
+      userId: 'Utilisateur5',
+      content: "très belles chambres, j'adore!.",
+      createdDate: new Date('2024-11-24'),
+      rating: 4,
+      replies: [],
+    },
+  ];
 
+  statutsFiltres: string[] = ["Vue d'ensemble", 'Hotellerrie', 'Tourisme'];
+  selectedStatut: string = "Vue d'ensemble"; // Valeur par défaut
 
+  sousStatuts: { [key: string]: string[] } = {
+    Hotellerrie: ['Hotel (s)', 'Motel (s)', 'Appartement (s)'],
+    Tourisme: [
+      'Autres',
+      'Randonnés',
+      'Parcs',
+      'Campings',
+      'Plages privées',
+      'Maisons traditionnelles',
+    ],
+  };
 
-    statutsFiltres: string[] = ['Vue d\'ensemble', 'Hotellerrie', 'Tourisme'];
-    selectedStatut: string = 'Vue d\'ensemble'; // Valeur par défaut
-    
-    sousStatuts: { [key: string]: string[] } = {
-      'Hotellerrie': ['Hotel (s)', 'Motel (s)','Appartement (s)'],
-      'Tourisme': ['Autres', 'Randonnés','Parcs','Campings','Plages privées','Maisons traditionnelles']
-    };
-    
-    // Liste des évaluations pour le filtre
-    evaluationsFiltres: string[] = ['Tout grouper', 'Très bon', 'Bon', 'Moyen', 'Mauvais'];
-    selectedEvaluation: string = 'Tout grouper'; // Valeur par défaut
+  // Liste des évaluations pour le filtre
+  evaluationsFiltres: string[] = [
+    'Tout grouper',
+    'Très bon',
+    'Bon',
+    'Moyen',
+    'Mauvais',
+  ];
+  selectedEvaluation: string = 'Tout grouper'; // Valeur par défaut
 
-    
   // Liste des durées pour le filtre
   dureesFiltres: string[] = ['Tous les années'];
   selectedDuree: string = 'Tous les années'; // Valeur par défaut
- 
 
-  dialogueMessage : boolean=false;
-  dialogueMessageEnvoye:boolean=false
-  estBloque:boolean = false
-  estSupprimer:boolean = false
+  dialogueMessage: boolean = false;
+  dialogueMessageEnvoye: boolean = false;
+  estBloque: boolean = false;
+  estSupprimer: boolean = false;
+  messagebloque: string = '';
+  selectedValue: string = '';
 
   constructor() {
     // Initialiser le sous-statut en fonction du statut par défaut
@@ -236,164 +242,205 @@ export class AccueilProComponent implements OnInit, OnDestroy {
   }
 
   initializeCountryFilters() {
-    const uniqueCountries = new Set(this.clients.map(client => client.pays));
+    const uniqueCountries = new Set(this.clients.map((client) => client.pays));
     this.paysFiltres.push(...Array.from(uniqueCountries));
   }
 
   initializeDurationFilters() {
-    const uniqueDurations = new Set(this.clients.map(client => client.duree));
+    const uniqueDurations = new Set(this.clients.map((client) => client.duree));
     this.dureesFiltres.push(...Array.from(uniqueDurations)); // Ajoute "Tous" et les durées uniques
   }
 
   get filteredClients() {
-    return this.clients.filter(client => {
-      const matchesPays = this.selectedPays === 'Tous les pays' || client.pays === this.selectedPays;
-      const matchesEvaluation = this.selectedEvaluation === 'Tout grouper' || client.evaluation === this.selectedEvaluation;
-      const matchesDuree = this.selectedDuree === 'Tous les années' || client.duree === this.selectedDuree;
+    return this.clients.filter((client) => {
+      const matchesPays =
+        this.selectedPays === 'Tous les pays' ||
+        client.pays === this.selectedPays;
+      const matchesEvaluation =
+        this.selectedEvaluation === 'Tout grouper' ||
+        client.evaluation === this.selectedEvaluation;
+      const matchesDuree =
+        this.selectedDuree === 'Tous les années' ||
+        client.duree === this.selectedDuree;
 
       return matchesPays && matchesEvaluation && matchesDuree;
       // return matchesPays && matchesEvaluation;
     });
   }
 
+  selectedSousStatut: string = ''; // Initialiser comme une chaîne vide
+  filterPays: string = ''; // Propriété pour le filtre
+  dateDebut: string = '';
+  dateFin: string = '';
+  paysFiltres: string[] = ['Tous les pays'];
+  selectedPays: string = 'Tous les pays'; // Valeur par défaut
+  showCheckboxes: boolean = false;
+  selectedStatuts: string[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 5;
+  itemsPerPageOptions: number[] = [5, 10, 50];
+  selectedClient: any = null;
+  showDetails: boolean = false;
+  numeroTelephone: boolean = false;
+  checkboxValues: { [key: string]: boolean } = {};
+  openFiltre: boolean = false;
 
-
-
-    selectedSousStatut: string = ''; // Initialiser comme une chaîne vide
-    filterPays: string = ''; // Propriété pour le filtre
-    dateDebut: string = '';
-    dateFin: string = '';
-    paysFiltres: string[] = ['Tous les pays'];
-    selectedPays: string = 'Tous les pays'; // Valeur par défaut
-    showCheckboxes: boolean = false;
-    selectedStatuts: string[] = [];
-    currentPage: number = 1;
-    itemsPerPage: number = 5;
-    itemsPerPageOptions: number[] = [5, 10, 50];
-    selectedClient: any = null;
-    showDetails: boolean = false;
-    numeroTelephone:boolean = false;
-    checkboxValues: { [key: string]: boolean } = {};
-    openFiltre: boolean = false;
-
-    afficherFiltre() {
+  afficherFiltre() {
     this.openFiltre = !this.openFiltre;
   }
 
-    // labels = [
-    //   { for: 'statut', text: 'Apperçu ' },
-    //   { for: 'pays', text: 'Région ' },
-    //   { for: 'evaluation', text: 'Evaluation ' },
-    //   { for: 'duree', text: 'Filtre par année ' },
-    //   { for: 'filtre', text: 'Filtre ' }
-    // ];
+  // labels = [
+  //   { for: 'statut', text: 'Apperçu ' },
+  //   { for: 'pays', text: 'Région ' },
+  //   { for: 'evaluation', text: 'Evaluation ' },
+  //   { for: 'duree', text: 'Filtre par année ' },
+  //   { for: 'filtre', text: 'Filtre ' }
+  // ];
 
-    // Fonction pour obtenir la classe dynamique
-    // getLabelClass(index: number): string {
-    //   switch(index) {
-    //     case 0:
-    //       return 'label-style-1';
-    //     case 1:
-    //       return 'label-style-2';
-    //     case 2:
-    //       return 'label-style-3';
-    //     case 3:
-    //       return 'label-style-4';
-    //     case 4:
-    //       return 'label-style-5';
-    //     default:
-    //       return '';
-    //   }
-    // }
+  // Fonction pour obtenir la classe dynamique
+  // getLabelClass(index: number): string {
+  //   switch(index) {
+  //     case 0:
+  //       return 'label-style-1';
+  //     case 1:
+  //       return 'label-style-2';
+  //     case 2:
+  //       return 'label-style-3';
+  //     case 3:
+  //       return 'label-style-4';
+  //     case 4:
+  //       return 'label-style-5';
+  //     default:
+  //       return '';
+  //   }
+  // }
 
-    getClientsFiltres() {
-        return this.clients.filter(client => {
-            const dateArrivee = new Date(client.statut);
-            const dateDebutObj = new Date(this.dateDebut);
-            const dateFinObj = new Date(this.dateFin);
+  getClientsFiltres() {
+    return this.clients.filter((client) => {
+      const dateArrivee = new Date(client.statut);
+      const dateDebutObj = new Date(this.dateDebut);
+      const dateFinObj = new Date(this.dateFin);
 
-            const isStatutValide = this.selectedStatut === 'Vue d\'ensemble' || client.statut === this.selectedStatut;
-            const isDateValide = (!this.dateDebut || dateArrivee >= dateDebutObj) && (!this.dateFin || dateArrivee <= dateFinObj);
+      const isStatutValide =
+        this.selectedStatut === "Vue d'ensemble" ||
+        client.statut === this.selectedStatut;
+      const isDateValide =
+        (!this.dateDebut || dateArrivee >= dateDebutObj) &&
+        (!this.dateFin || dateArrivee <= dateFinObj);
 
-            return isStatutValide && isDateValide;
-        });
-    }
+      return isStatutValide && isDateValide;
+    });
+  }
 
-    printPage() {
-        window.print();
-    }
+  printPage() {
+    window.print();
+  }
 
-    toggleCheckboxes() {
-      this.showCheckboxes = !this.showCheckboxes;
-    }
+  toggleCheckboxes() {
+    this.showCheckboxes = !this.showCheckboxes;
+  }
 
-    toggleStatutSelection(statut: string, event: Event) {
+  toggleStatutSelection(statut: string, event: Event) {
     const checkbox = event.target as HTMLInputElement;
     if (checkbox.checked) {
-        this.selectedStatuts.push(statut);
+      this.selectedStatuts.push(statut);
     } else {
-        this.selectedStatuts = this.selectedStatuts.filter(s => s !== statut);
+      this.selectedStatuts = this.selectedStatuts.filter((s) => s !== statut);
     }
     console.log(this.selectedStatuts); // Pour déboguer et voir les statuts sélectionnés
+  }
+
+  get paginatedClients() {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    return this.filteredClients.slice(start, start + this.itemsPerPage); // Appliquer le filtrage ici
+  }
+
+  get totalPages() {
+    return Math.ceil(this.getClientsFiltres().length / this.itemsPerPage);
+  }
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
     }
+  }
 
-    get paginatedClients() {
-      const start = (this.currentPage - 1) * this.itemsPerPage;
-      return this.filteredClients.slice(start, start + this.itemsPerPage); // Appliquer le filtrage ici
+  onItemsPerPageChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.itemsPerPage = Number(selectElement.value);
+    this.currentPage = 1; // Réinitialiser à la première page
+  }
+
+  // Méthode appelée lors du clic sur le lien
+  afficherDetails(client: Client) {
+    this.selectedClient = client;
+    this.showDetails = true; // On affiche la div conditionnée
+  }
+
+  afficherTelephone() {
+    this.numeroTelephone = !this.numeroTelephone;
+  }
+
+  // Méthode pour revenir à la vue par défaut
+  revenir() {
+    this.selectedClient = null;
+    this.showDetails = false; // On cache la div conditionnée
+  }
+
+  toogleMessage() {
+    this.dialogueMessage = !this.dialogueMessage;
+  }
+
+  messageenvoye() {
+    this.dialogueMessage = !this.dialogueMessage;
+    this.dialogueMessageEnvoye = !this.dialogueMessageEnvoye;
+  }
+  fermerMessageenvoye() {
+    this.dialogueMessageEnvoye = !this.dialogueMessageEnvoye;
+  }
+  estBloqueEtablissement() {
+    this.estBloque = !this.estBloque;
+  }
+
+  estSupprimeEtablissement() {
+    this.estSupprimer = !this.estSupprimer;
+  }
+
+  onRadioChange(event: any) {
+    // this.estBloque = !this.estBloque;
+    this.selectedValue = event.target.value;
+  }
+
+  onEnregisterClick() {
+    if (this.selectedValue) {
+      const dateDebut = new Date().toLocaleDateString();
+      const dateFin = this.calculerDateFin(this.selectedValue);
+      this.messagebloque = `Etablissement bloqué temporairement  le ${dateDebut} au ${dateFin}.          Durée : ${this.selectedValue}`;
+      this.estBloque = !this.estBloque;
+    } else {
+      this.messagebloque = 'Veuillez sélectionner une durée de blocage.';
     }
+  }
 
-    get totalPages() {
-      return Math.ceil(this.getClientsFiltres().length / this.itemsPerPage);
+  calculerDateFin(valeur: string): string {
+    const date = new Date();
+    switch (valeur) {
+      case '1 semaine':
+        date.setDate(date.getDate() + 7);
+        break;
+      case '2 semaines':
+        date.setDate(date.getDate() + 14);
+        break;
+      case '1 mois':
+        date.setMonth(date.getMonth() + 1);
+        break;
+      case '2 mois':
+        date.setMonth(date.getMonth() + 2);
+        break;
+      case '3 mois':
+        date.setMonth(date.getMonth() + 3);
+        break;
     }
-
-    goToPage(page: number) {
-      if (page >= 1 && page <= this.totalPages) {
-        this.currentPage = page;
-      }
-    }
-
-    onItemsPerPageChange(event: Event) {
-      const selectElement = event.target as HTMLSelectElement;
-      this.itemsPerPage = Number(selectElement.value);
-      this.currentPage = 1; // Réinitialiser à la première page
-    }
-
-
-    // Méthode appelée lors du clic sur le lien
-    afficherDetails(client: Client) {
-      this.selectedClient = client;
-      this.showDetails = true; // On affiche la div conditionnée
-    }
-
-    afficherTelephone(){
-      this.numeroTelephone = !this.numeroTelephone
-    }
-
-      // Méthode pour revenir à la vue par défaut
-    revenir() {
-      this.selectedClient  = null;
-      this.showDetails = false; // On cache la div conditionnée
-    }
-
-    toogleMessage(){
-      this.dialogueMessage = !this.dialogueMessage
-    }
-
-    messageenvoye(){
-      this.dialogueMessage = !this.dialogueMessage
-      this.dialogueMessageEnvoye = !this.dialogueMessageEnvoye
-
-    }
-    fermerMessageenvoye(){
-      this.dialogueMessageEnvoye = !this.dialogueMessageEnvoye
-
-    }
-    estBloqueEtablissement(){
-      this.estBloque=!this.estBloque
-    }
-
-    estSupprimeEtablissement(){
-      this.estSupprimer=!this.estSupprimer
-    }
-
+    return date.toLocaleDateString();
+  }
 }
