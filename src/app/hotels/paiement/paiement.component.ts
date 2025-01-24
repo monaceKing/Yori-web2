3cb14250e1581737242933d1ab7d5bf20f6c4e35
devIgf <igfdev0@gmail.com>
2025-01-24 14:46:14 +0000

    rajout côté établissement

--- src/app/hotels/paiement/paiement.component.ts
+++ src/app/hotels/paiement/paiement.component.ts
@@ -23,7 +23,7 @@ export class PaiementComponent implements OnInit{
   ngOnInit() {}
 
   items = [
-    { label: 'aucune de carte pour le moment', checked: false,},
+    { label: 'aucune de carte pour le moment', checked: false},
     { label: 'Carte de crédit/débit', checked: false, images: ['assets/img/visa.png', 'assets/img/visa-yellow.png', 'assets/img/GB-card.png'] },
     { label: 'Paiement digital', checked: false, images: ['assets/img/paypal.png'] },
     { label: 'Paysing', checked: false, images: ['assets/img/GB-card.png'] },
