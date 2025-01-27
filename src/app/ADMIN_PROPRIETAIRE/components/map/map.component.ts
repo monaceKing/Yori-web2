98981134c23ca47e79de9eb38b5f067ba7e2eafc
MonaceAkg <justeamour05@gmail.com>
2025-01-27 08:29:42 +0000

    début d'ajout du style côté ajout d'établissements

--- src/app/ADMIN_PROPRIETAIRE/components/map/map.component.ts
+++ src/app/ADMIN_PROPRIETAIRE/components/map/map.component.ts
@@ -11,20 +11,39 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements AfterViewInit {
   ngAfterViewInit(): void {
-    // Initialisation de la carte
-    const map = L.map('map').setView([48.8566, 2.3522], 13); // Coordonnées pour Paris
-
+    // Initialisation de la carte avec un centre temporaire
+    const map = L.map('map').setView([0, 0], 13);
+  
     // Ajout des tuiles OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
       maxZoom: 19,
     }).addTo(map);
-
-    // Ajout d'un marqueur
-    L.marker([48.8566, 2.3522])
-      .addTo(map)
-      .bindPopup('Voici votre localisation !')
-      .openPopup();
+  
+    // Vérifier si la géolocalisation est disponible
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+  
+          // Mettre à jour la vue de la carte avec la position de l'utilisateur
+          map.setView([latitude, longitude], 13);
+  
+          // Ajouter un marqueur à la position actuelle
+          L.marker([latitude, longitude])
+            .addTo(map)
+            .bindPopup('Vous êtes ici !')
+            .openPopup();
+        },
+        (error) => {
+          console.error('Erreur de géolocalisation :', error);
+          alert('Impossible de récupérer votre position actuelle.');
+        }
+      );
+    } else {
+      alert('La géolocalisation n\'est pas supportée par votre navigateur.');
+    }
   }
+  
 }
--- src/app/etablissements/choix-etablissement/choix-etablissement.component.ts
+++ src/app/etablissements/choix-etablissement/choix-etablissement.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-choix-etablissement',
   standalone: true,
-  imports: [MatMenu, MatMenuTrigger, RouterLink],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './choix-etablissement.component.html',
   styleUrl: './choix-etablissement.component.css'
 })
